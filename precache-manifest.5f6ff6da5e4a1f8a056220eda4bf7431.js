8a2455b8db162e642e8c5715ed3d7fd37fb98359
Ashraf <54310457+afaithraf@users.noreply.github.com>
2019-12-27 21:35:42 +0100

    Updates

---
+++ precache-manifest.5f6ff6da5e4a1f8a056220eda4bf7431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dc15898d84cd7fc3cdea07cc608c53",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5eb804bcad46379a42d6",
+    "url": "/portfolio/static/css/main.1bd8481f.chunk.css"
+  },
+  {
+    "revision": "b6ac804deb12ef5ba720",
+    "url": "/portfolio/static/js/2.a5e98536.chunk.js"
+  },
+  {
+    "revision": "5d83b32028c822b364148904e89bb0e7",
+    "url": "/portfolio/static/js/2.a5e98536.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5eb804bcad46379a42d6",
+    "url": "/portfolio/static/js/main.c1149b1d.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  }
+]);
